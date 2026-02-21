6287c3cde231f2a5ca7271ac9e24d537a3402e8d
Kristian <info@edvcostruzioni.com>
2026-02-21 15:55:32 +0100

    feat: modulo amministrazione14 4

--- utils/data-fetcher.ts
+++ utils/data-fetcher.ts
@@ -1099,7 +1099,7 @@ export async function getAgingAnalysis() {
 export async function getKPIFinanziariGlob() {
   const supabase = createClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+    process.env.SUPABASE_SERVICE_ROLE_KEY!
   );
   
   const { data: params } = await supabase
